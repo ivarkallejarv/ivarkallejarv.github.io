fb00378e6595e9b47fec43f3161ac1fcd35270d9
phoenix913 <ivarkallejarv@khk.ee>
2016-10-02 14:12:50 +0300

    Some simple bootstrap
    Blah

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*
+(function(){
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+}());
+*/
+
+$(document).ready(function(){
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+});
